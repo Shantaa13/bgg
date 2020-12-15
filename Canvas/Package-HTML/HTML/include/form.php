7bddacc2f350fa844e9a9f54d76e77696aa00c82
Shantaa13 <75464900+Shantaa13@users.noreply.github.com>
2020-12-15 11:31:18 +0700

    3

--- Canvas/Package-HTML/HTML/include/form.php
+++ Canvas/Package-HTML/HTML/include/form.php
@@ -1,5 +1,5 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
 /*-------------------------------------------------
 
 	Form Processor Plugin
@@ -27,8 +27,8 @@ require 'phpmailer/src/SMTP.php';
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'your-email@website.com', // Your Email Address
-				'name' => 'Your Name' // Your Name
+				'email' => 'mggbangkok@medgicgroup.com', // Your Email Address
+				'name' => 'Admin' // Your Name
 			);
 
 
@@ -37,8 +37,8 @@ $toemails[] = array(
 ---------------------------------------------------*/
 
 $fromemail = array(
-				'email' => 'no-reply@website.com', // Company's Email Address (preferably currently used Domain Name)
-				'name' => 'Company Name' // Company Name
+				'email' => 'no-reply@medgicgroup.com', // Company's Email Address (preferably currently used Domain Name)
+				'name' => 'BGG' // Company Name
 			);
 
 
